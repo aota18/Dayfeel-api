6cbeba7f439e0bca3b5a97ceceace5942f3c6fff
aota18 <sapphire031794@gmail.com>
2021-08-10 18:24:39 +0900

    self-signed ssl

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,13 @@ import * as fs from 'fs';
 
 async function bootstrap() {
 
-  const httpsOptions = {
-    key: fs.readFileSync('src/ssl/private.pem'),
-    cert: fs.readFileSync('src/ssl/public.pem')
-  }
+
 
   const app = await NestFactory.create(
     AppModule, 
     {
       cors: true,
-      httpsOptions
+     
     });
   await app.listen(3000);
 }
