45c21c54414c731efd37069447d5cd7cdc07c91a
aota18 <sapphire031794@gmail.com>
2021-08-14 22:05:15 +0900

    https updates

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,19 @@ import { AppModule } from './app.module';
 import * as fs from 'fs';
 
 async function bootstrap() {
+  
 
+  let httpsOptions = process.env.NODE_ENV==='development' ? {} : {
+    key: fs.readFileSync('/usr/share/private.pem'),
+    cert: fs.readFileSync('/usr/share/fullchain.pem')
+  }
 
 
   const app = await NestFactory.create(
     AppModule, 
     {
       cors: true,
-     
+       httpsOptions
     });
   await app.listen(3000);
 }
